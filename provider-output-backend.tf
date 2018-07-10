158c2a6b44a54c8136a4fe95116adfaa00ce0a9a
bronsonberwald <40666265+bronsonberwald@users.noreply.github.com>
2018-07-10 13:37:12 -0500

    Update provider-output-backend.tf

--- provider-output-backend.tf
+++ provider-output-backend.tf
@@ -2,4 +2,10 @@ provider "aws" {
 #  shared_credentials_file  = "${var.cred-file}" # we don't use it in CodeBuild
   profile    = "${var.profile}"
   region     = "${var.region}"
+}terraform { 
+   backend "s3" { 
+   bucket = "..terraform-state-software"
+   key    = "terraform.tfstate"
+   region = ""
+  }
 }
