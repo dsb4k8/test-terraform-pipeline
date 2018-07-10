0aa8d782f0870f9adb2c74a67f75ccc667ed31f4
bronsonberwald <40666265+bronsonberwald@users.noreply.github.com>
2018-07-10 14:29:04 -0500

    Update provider-output-backend.tf

--- provider-output-backend.tf
+++ provider-output-backend.tf
@@ -1,5 +1,7 @@
 provider "aws" {
 #  shared_credentials_file  = "${var.cred-file}" # we don't use it in CodeBuild
   profile    = "${var.profile}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   region     = "${var.region}"
 }
